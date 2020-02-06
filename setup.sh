afd16591498116dc71287dac254bba9c44cfbef3
Jixin Jia <jixjia@microsoft.com>
2020-02-06 16:42:08 +0900

    updated setup.py

--- setup.sh
+++ setup.sh
@@ -1,4 +1,44 @@
 #!/bin/bash
+while [[ $# -gt 0 ]]
+do
+    case "$1" in
+        -g|--resource-group)
+            AZRGNAME="$2"
+            shift 2
+            ;;
+        -l|--location)
+            AZLOCATION="$2"
+            shift 2
+            ;;
+        -n|-n)
+            AZAPPNAME="$2"
+            shift 2
+            ;;
+        -p|--plan)
+            AZAPPPLAN="$2"
+            shift 2
+            ;;
+        -t|--tenantid)
+            AZTENANTID="$2"
+            shift 2
+            ;;
+        -a|--appid)
+            AZAPPID="$2"
+            shift 2
+            ;;
+        -z|--zipfolder)
+            ZIPFOLDER="$2"
+            shift 2
+            ;;
+        -b|--buildtag)
+            BUILDTAG="$2"  
+            shift 2
+            ;;
+    esac
+done
+
+echo "RG=$AZRGNAME, LOCATION=$AZLOCATION, PLAN=$AZAPPPLAN, APP=$AZAPPNAME, ZIPFOLDER=$ZIPFOLDER, tenantid=$AZTENANTID, AppID=$AZAPPID, BuildTag=$BUILDTAG"
+
 rm -f ./deploy.zip
 zip -r ./deploy.zip $ZIPFOLDER -x ./.git\* ./.vscode\* ./deploy\*
 
